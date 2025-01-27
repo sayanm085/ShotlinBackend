2fe824f5006a65fc0e5b19de194305a5f67c1924
Sayan Mondal <154863207+sayanm085@users.noreply.github.com>
2025-01-27 12:22:53 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,51 +1,38 @@
 import connectDB from "./db/Database.js";
 import app from "./app.js";
-import dotenv from 'dotenv';
-import os from 'os';
+import os from "os";
 import cluster from "cluster";
 import { PORT } from "./constants.js";
 
-dotenv.config();
-
-// let totalCPUs = os.cpus().length;
-// console.log(`Total CPUs: ${totalCPUs}`);
-
-// if(cluster.isMaster){
-//     const cpus = os.cpus().length;
-//     for(let i = 0; i < cpus; i++){
-//         cluster.fork();
-//     }
-//     cluster.on('exit', (worker, code, signal) => {
-//         console.log(`Worker ${worker.process.pid} died`);
-//         cluster.fork();
-//     });
-
-// }
-
-// if(cluster.isWorker){
-//     connectDB()
-//     .then(() => {
-//         app.listen(process.env.PORT || 8000, () => {
-//             console.log(`Server is running on port http://localhost:${process.env.PORT} ||8000`);
-//         });
-//     })
-//     .catch((error) => {
-//         console.log("error in db connection" , error);
-//         process.exit(1);
-//     });
-// }
 
 
+// Get the total number of CPUs
+const totalCPUs = os.cpus().length;
 
+if (cluster.isMaster) {
+    console.log(`Master process running. Total CPUs: ${totalCPUs}`);
 
+    // Fork workers for each CPU
+    for (let i = 0; i < totalCPUs; i++) {
+        cluster.fork();
+    }
 
-connectDB()
-.then(() => {
-    app.listen(PORT || 8000, () => {
-        console.log(`Server is running on port http://localhost:${PORT} ||8000`);
+    // Restart a worker if it dies
+    cluster.on("exit", (worker, code, signal) => {
+        console.error(`Worker ${worker.process.pid} exited. Restarting...`);
+        cluster.fork();
     });
-})
-.catch((error) => {
-    console.log("error in db connection" , error);
-    process.exit(1);
-});
+} else {
+    // Worker process: connect to the database and start the server
+    connectDB()
+        .then(() => {
+            const port = PORT || 8000;
+            app.listen(port, () => {
+                console.log(`Worker ${process.pid} running on http://localhost:${port}`);
+            });
+        })
+        .catch((error) => {
+            console.error("Database connection failed:", error);
+            process.exit(1); // Exit process if the database connection fails
+        });
+}
