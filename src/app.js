792ee30ae422ef2b278e061d32b4c3d51584d6de
Sayan Mondal <154863207+sayanm085@users.noreply.github.com>
2025-02-03 12:31:45 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -7,14 +7,12 @@ import crypto from 'crypto';
 // Create the Express app
 let app = express();
 
-// app.use(cors({
-//     origin: 'http://localhost:5173',  // Allow all origins
-//     credentials: true,  // Allow cookies to be sent with requests
-//     allowedHeaders: ['Content-Type', 'Authorization '], // Allow the Authorization header to be sent
-//     allowedMethods: ['GET', 'POST', 'PUT', 'DELETE', ] // Allow the GET, POST, PUT, DELETE methods
-// }));
-
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173',"https://shotlin.in","https://shotlin.com"],  // Allow the React app to connect to the server  (URL of the React app)
+    credentials: true,  // Allow cookies to be sent with requests
+    allowedHeaders: ['Content-Type', 'Authorization '], // Allow the Authorization header to be sent
+    allowedMethods: ['GET', 'POST', 'PUT', 'DELETE', ] // Allow the GET, POST, PUT, DELETE methods
+}));
 
 app.use(express.json({limit: "16kb"}))
 app.use(express.urlencoded({extended: true, limit: "16kb"}))
